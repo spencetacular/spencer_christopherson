b70b8c95aba2b6a4285db5c3e1def4ea351d4b21
Spencer Christopherson <schristopherson@gmail.com>
2016-04-26 13:24:58 -0600

    Added finished table to db and finished fields to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423211620) do
+ActiveRecord::Schema.define(version: 20160426180524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160423211620) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "technology"
+    t.boolean  "finished"
   end
 
   create_table "tag_names", force: :cascade do |t|
