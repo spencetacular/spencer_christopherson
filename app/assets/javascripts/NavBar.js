4468629e63af07cfd51cb8f4a093eab9c44ea0e5
Spencer Christopherson <schristopherson@gmail.com>
2016-04-26 11:59:52 -0600

    Nav bar updates and working projects

--- app/assets/javascripts/NavBar.js
+++ app/assets/javascripts/NavBar.js
@@ -1,6 +1,16 @@
 $( document ).ready(function(){
 
  $(".button-collapse").sideNav();
+ $(".dropdown-button").dropdown();
+
+ // $("#portfolio-button").click(function(){
+ // 	$("#subtitle").text("changed");
+ // 	alert('Hello button clicked!');
+ // });
+
+ // $('#say_hello').click(function(){
+ //    alert('Hello button clicked!');
+ //  });
 
 })
  
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,9 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update, :destroy,]
   
   def index
   	@projects = Project.all
+  
   end
 
   def show
